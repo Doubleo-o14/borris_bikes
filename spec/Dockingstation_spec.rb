77449ad6e72b43e48a8bf3be5a549db3a885711e
olaleye <olaoduola01@gmail.com>
2021-10-20 15:41:53 +0100

    adding working

--- spec/Dockingstation_spec.rb
+++ spec/Dockingstation_spec.rb
@@ -3,6 +3,12 @@ describe Dockingstation do
     it "can take out bike" do
         docking_station = Dockingstation.new
         expect(docking_station).to respond_to :release_bike
+    end
+
+    it "can tell us if bike is working" do
+      docking_station = Dockingstation.new
+      bike  = docking_station.release_bike
+      expect(bike).to be_working
   end
 end
         
