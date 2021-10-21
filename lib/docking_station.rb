88d0f3f4f518b92afbecd0b91c41643d8839f7de
olaleye <olaoduola01@gmail.com>
2021-10-21 15:47:37 +0100

    private

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,23 @@ class Dockingstation
     end
 
     def release_bike
-      raise 'no bikes available' unless !@bike_storage.empty?
+      empty?
       @bike_storage[0]
     end
     
     def dock(bike)
-      raise 'capacity is full' if @bike_storage.length >= 20
+      full?
       @bike_storage << bike
     end
+
+    private
+
+    def full?
+      raise 'capacity is full' if @bike_storage.length >= 20
+    end
+    
+    def empty?
+      raise 'no bikes available' unless !@bike_storage.empty?
+    end
+
 end
